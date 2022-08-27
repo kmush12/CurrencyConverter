158775b744f2279f9e3f1aee69e7b20800bd73d8
kmush12 <256376@student.pwr.edu.pl>
2022-08-27 15:26:27 +0200

    Initial Commit

---
+++ src/test/java/com/kmush12/CurrencyConverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmush12.CurrencyConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
